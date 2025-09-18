2c44dd3bd7ba4580ccc48f9c49b9b6b8a6b123b1
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-09-18 17:51:51 +0100

    changes

--- app/api/featured-work/route.ts
+++ app/api/featured-work/route.ts
@@ -22,14 +22,14 @@ const featureWork = [
         image: "/images/feature-work/feature-img-3.png"
 
     },
-    /*     {
-            title: "Fullstack Developer at Thrive Agric",
-            description: "Thrive Agric is an agritech company based in Nigeria. Its mission is to build a network of profitable smallholder farmers by giving them access to inputs, finance, best practices, and markets.",
-            roles: ["Backend Developer", "Devops Engineer"],
-            image: "/images/feature-work/feature-img-4.png"
-    
-    
-        } */
+    {
+        title: "Fullstack Developer at Thrive Agric",
+        description: "Thrive Agric is an agritech company based in Nigeria. Its mission is to build a network of profitable smallholder farmers by giving them access to inputs, finance, best practices, and markets.",
+        roles: ["Backend Developer", "Devops Engineer"],
+        image: "/images/feature-work/feature-img-4.png"
+
+
+    }
 ]
 
 export const GET = async () => {
