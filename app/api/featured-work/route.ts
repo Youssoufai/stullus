cf3f6aa25e235471ce2f0341ebf2ca5948e99ca6
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-09-18 17:50:31 +0100

    lead

--- app/api/featured-work/route.ts
+++ app/api/featured-work/route.ts
@@ -8,12 +8,12 @@ const featureWork = [
         image: "/images/feature-work/feature-img-1.png"
         /* Image */
     },
-    /*     {
-            title: "Lead Developer at HelthCloud",
-            description: "Created a distinctive visual identity and design language to build trust and empathy for a forward-thinking health care provider.",
-            roles: ["Backend Developer", "Mobile Developer"],
-            image: "/images/feature-work/feature-img-2.png"ghghgh
-        }, */
+    {
+        title: "Lead Developer at HelthCloud",
+        description: "Created a distinctive visual identity and design language to build trust and empathy for a forward-thinking health care provider.",
+        roles: ["Backend Developer", "Mobile Developer"],
+        image: "/images/feature-work/feature-img-2.png"
+    },
 
     {
         title: "Fullstack Developer at Kids World",
@@ -22,14 +22,14 @@ const featureWork = [
         image: "/images/feature-work/feature-img-3.png"
 
     },
-    {
-        title: "Fullstack Developer at Thrive Agric",
-        description: "Thrive Agric is an agritech company based in Nigeria. Its mission is to build a network of profitable smallholder farmers by giving them access to inputs, finance, best practices, and markets.",
-        roles: ["Backend Developer", "Devops Engineer"],
-        image: "/images/feature-work/feature-img-4.png"
-
-
-    }
+    /*     {
+            title: "Fullstack Developer at Thrive Agric",
+            description: "Thrive Agric is an agritech company based in Nigeria. Its mission is to build a network of profitable smallholder farmers by giving them access to inputs, finance, best practices, and markets.",
+            roles: ["Backend Developer", "Devops Engineer"],
+            image: "/images/feature-work/feature-img-4.png"
+    
+    
+        } */
 ]
 
 export const GET = async () => {
